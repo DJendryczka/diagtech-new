292017def3d748ff04eacb2ef50267b603817b14
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2023-03-06 17:29:31 +0100

    Added about section

--- script.js
+++ script.js
@@ -10,11 +10,14 @@ const navSlide = () => {
 
 const navChangeColor = () => {
     const nav = document.getElementById('navbar')
+    const navText = document.getElementById('nav-text')
     console.log('zmiana')
     if(window.scrollY >=95){
         nav.classList.add('bg-white')
+        navText.classList.add('text-gray-600')
     }else{
         nav.classList.remove('bg-white')
+        navText.classList.remove('text-gray-600')
     }
 }
 

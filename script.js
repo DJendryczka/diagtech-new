c4c4a952a3309261eac0ea89e0c13c1790ba4e64
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2023-03-09 20:07:04 +0100

    Function serviceInfoToggler ready but dry

--- script.js
+++ script.js
@@ -32,13 +32,37 @@ navSlide()
 const servicesInfoToggler = () => {
   const info = document.getElementById('info')
   const webb = document.getElementById('webb')
+  const design = document.getElementById('design')
+  const hosting = document.getElementById('hosting')
+  const ceo = document.getElementById('ceo')
   const button = document.querySelectorAll("#btn");
   for (let i = 0; i < button.length; i++) {
     button[i].addEventListener("click", (e) => {
       console.log("Button " + (i) + " was clicked");
       if(e.target.outerText === 'Webbutveckling'){
-        info.classList.toggle('hidden')
-        webb.classList.toggle('hidden')
+        info.classList.add('hidden')
+        webb.classList.remove('hidden')
+        design.classList.add('hidden')
+        hosting.classList.add('hidden')
+        ceo.classList.add('hidden')
+      }else if(e.target.outerText === 'Design & UX'){
+        info.classList.add('hidden')
+        design.classList.remove('hidden')
+        webb.classList.add('hidden')
+        hosting.classList.add('hidden')
+        ceo.classList.add('hidden')
+      }else if(e.target.outerText === 'Hosting & förvaltning'){
+        info.classList.add('hidden')
+        design.classList.add('hidden')
+        webb.classList.add('hidden')
+        hosting.classList.remove('hidden')
+        ceo.classList.add('hidden')
+      }else{
+        info.classList.add('hidden')
+        design.classList.add('hidden')
+        webb.classList.add('hidden')
+        ceo.classList.remove('hidden')
+        hosting.classList.add('hidden')
       }
     });
   }
